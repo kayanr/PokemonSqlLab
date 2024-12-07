4f52814fd4d64daa3cb43638742703e5a22e9778
Kayan Reid <kreid@Zipcoders-MBP-4.fios-router.home>
2024-12-07 16:31:42 -0500

    Added my answers for part3.

--- answers/part3_joins_groups.sql
+++ answers/part3_joins_groups.sql
@@ -1,10 +1,16 @@
-/*What is each pokemon's primary type? */
+--What is each pokemon's primary type?
+   SELECT p.id, p.name AS Pokemon_Name, t.name AS Primary_Type FROM pokemons p
+    join types t on p.primary_type = t.id;
 
+--What is Rufflet's secondary type? */
+   SELECT p.id, p.name AS Pokemon_Name, t.name AS Secondary_Type FROM pokemons p join types t on p.secondary_type = t.id WHERE p.name = 'Rufflet';
 
-/*What is Rufflet's secondary type? */
+--What are the names of the pokemon that belong to the trainer with trainerID 303? */
 
-/*What are the names of the pokemon that belong to the trainer with trainerID 303? */
-/*How many pokemon have a secondary type Poison?*/
-/*What are all the primary types and how many pokemon have that type?*/
-/*How many pokemon at level 100 does each trainer with at least one level 100 pokemone have? (Hint: your query should not display a trainer*/
-/*How many pokemon only belong to one trainer and no other?*/
+--How many pokemon have a secondary type Poison?*/
+    SELECT count(*) FROM pokemons p join types t on p.secondary_type = t.id WHERE t.name = 'Poison';
+
+--What are all the primary types and how many pokemon have that type?*/
+
+--How many pokemon at level 100 does each trainer with at least one level 100 pokemone have? (Hint: your query should not display a trainer*/
+--How many pokemon only belong to one trainer and no other?*/
